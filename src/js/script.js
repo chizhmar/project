d16ee1bea7a691349756d5e4e82827026a6d59e6
Vitalik <chizhmar.vitalik1988@gmail.com>
2023-01-10 15:05:47 +0200

    add code

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,22 @@ $(document).ready(function(){
 		speed: 1200,
 		//adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/chevron-left-solid.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="img/icons/chevron right solid.png"></button>'
-	 });
+		nextArrow: '<button type="button" class="slick-next"><img src="img/icons/chevron right solid.png"></button>',
+		responsive: [
+			{
+				breakpoint: 991.98,
+				settings: {
+					dots: false,
+					arrows: false
+				}
+			}
+		]
+	});
 
-	 $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 		$(this)
-		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 		});
 
 		function toggleSlide(item) {
@@ -24,4 +33,4 @@ $(document).ready(function(){
 
 		toggleSlide('.catalog-item__link');
 		toggleSlide('.catalog-item__back');
- });
+	});
